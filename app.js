6d6527dd4c468184128fe777915e93547c50474b
Sidhi Anand <sidhianand31@gmail.com>
2025-08-31 14:53:52 +0530

    Add changes

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.use("/listings/:id/reviews", reviewRouter);
 app.use("/",userRouter);
 
 app.get("/", (req, res) => {
-  res.render("listings/index");
+  res.redirect("/listings");
 });
 
 //error handling middlewares
